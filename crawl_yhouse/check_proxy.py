5a9ba9708bd71b240c1c02c63ec1d154be0ca49e
chihiro <632207812@qq.com>
2019-05-21 16:45:37 +0800

    5/21 修改

--- crawl_yhouse/check_proxy.py
+++ crawl_yhouse/check_proxy.py
@@ -1,25 +1,25 @@
-from selenium import webdriver
-
-chromeOptions = webdriver.ChromeOptions()
-
-# 设置代理
-chromeOptions.add_argument("--proxy-server=http://112.65.52.112:4575")
-# 一定要注意，=两边不能有空格，不能是这样--proxy-server = http://202.20.16.82:10152
-browser = webdriver.Chrome(chrome_options=chromeOptions)
-
-# 查看本机ip，查看代理是否起作用
-browser.get("http://httpbin.org/ip")
-print(browser.page_source)
-
-
-# 退出，清除浏览器缓存
-# browser.quit()
-
-# import telnetlib
-# try:
-#   telnetlib.Telnet('106.111.45.220', port='4576', timeout=5)
-# except:
-#   print('connect failed')
-# else:
-#   print('success')
-
+# from selenium import webdriver
+#
+# chromeOptions = webdriver.ChromeOptions()
+#
+# # 设置代理
+# chromeOptions.add_argument("--proxy-server=http://112.65.52.112:4575")
+# # 一定要注意，=两边不能有空格，不能是这样--proxy-server = http://202.20.16.82:10152
+# browser = webdriver.Chrome(chrome_options=chromeOptions)
+#
+# # 查看本机ip，查看代理是否起作用
+# browser.get("http://httpbin.org/ip")
+# print(browser.page_source)
+#
+#
+# # 退出，清除浏览器缓存
+# # browser.quit()
+#
+# # import telnetlib
+# # try:
+# #   telnetlib.Telnet('106.111.45.220', port='4576', timeout=5)
+# # except:
+# #   print('connect failed')
+# # else:
+# #   print('success')
+#
