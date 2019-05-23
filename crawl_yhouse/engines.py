f05b4d62d06cebaf87092afdb3ad38a1249b13c9
chihiro <632207812@qq.com>
2019-05-23 11:36:06 +0800

    5/22 修改

--- crawl_yhouse/engines.py
+++ crawl_yhouse/engines.py
@@ -8,14 +8,14 @@ def get_engine(hostname):
     if hostname in ('wx09', 'wx01', 'wx02', 'wx03', 'wx04', 'wx05', 'wx06', 'wx07', 'wx08'):
         engine = create_engine(
             "mysql+pymysql://{}:{}@{}:{}/{}".format('yhouse', 'GHLs9X8q@QWkjo6#ZX4f', '10.10.209.30', 3306,
-                                                    'pytt', ), connect_args={"charset": "utf8"}, echo=True, )
+                                                    'test', ), connect_args={"charset": "utf8"}, echo=True, )
     else:
         # engine = create_engine(
         #     "mysql+pymysql://{}:{}@{}:{}/{}".format('spider_dev', 'csQXEMw9udrC4f*4JB6WnGiv2MKZRn', 'localhost', 13306,
         #                                             'pytt', ), connect_args={"charset": "utf8"}, echo=True, )
         engine = create_engine(
             "mysql+pymysql://{}:{}@{}:{}/{}".format('yhouse', 'GHLs9X8q@QWkjo6#ZX4f', 'localhost', 10002,
-                                                    'pytt', ), connect_args={"charset": "utf8"}, echo=True, )
+                                                    'test', ), connect_args={"charset": "utf8"}, echo=True, )
     return engine
 
 
