a324921787ab18acd6099c57e01ef5ddffddf807
chihiro <632207812@qq.com>
2019-05-29 10:10:31 +0800

    5/23 修改

--- crawl_yhouse/middlewares.py
+++ crawl_yhouse/middlewares.py
@@ -238,8 +238,8 @@ class CrawlYhouseDownloaderMiddleware(object):
 
 
             header = {
-                'User-Agent': random.choice(user_agent),
-                # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36',
+                # 'User-Agent': random.choice(user_agent),
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36',
                 'Referer': 'http://hotel.elong.com/%s/' %id,
                 'Origin': 'http://hotel.elong.com',
                 'Host': 'hotel.elong.com',
