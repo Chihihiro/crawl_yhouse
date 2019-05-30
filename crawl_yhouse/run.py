3a24aeabd753bedd611340b6c2ec157d2785f0d8
chihiro <632207812@qq.com>
2019-05-30 10:57:15 +0800

    5/23 修改

--- crawl_yhouse/run.py
+++ crawl_yhouse/run.py
@@ -1,6 +1,10 @@
 from scrapy import cmdline
 cmdline.execute("scrpay crawl yilong".split())
 
+#
+# import subprocess
+# subprocess.call(["ps -efww|grep Xvfb |grep -v grep|cut -c 9-15|xargs kill -9"])
 
-import subprocess
-subprocess.call(["ps -efww|grep Xvfb |grep -v grep|cut -c 9-15|xargs kill -9"])
+
+import os
+os.system("ps -efww|grep Xvfb |grep -v grep|cut -c 9-15|xargs kill -9")
